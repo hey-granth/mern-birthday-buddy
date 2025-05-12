5904ce330917bf9a6078231014cd1886483ed4b6
Granth Agarwal <heygranth@gmail.com>
2025-05-12 12:14:47 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,35 @@
+import data from './data';
+import { useState } from 'react';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+    const [people, setPeople] = useState(data);
+    const [isLoaded, setLoaded] = useState(true);
+
+    const handleClick = () => {
+        setLoaded(!isLoaded);
+        if (isLoaded) setPeople([]);
+        else setPeople(data);
+    };
+
+    return (
+        <>
+            <h2>{people.length} Birthday{people.length !== 1 ? 's' : ''} today</h2>
+            {
+                people.map((person) => {
+                    const { id, name, age, image } = person;
+                    return (
+                        <div key={id} className="person">
+                            <img src={image} alt={name} />
+                            <div>
+                                <h4>{name}</h4>
+                                <p>{age} years</p>
+                            </div>
+                        </div>
+                    );
+                })
+            }
+        </>
+    );
 };
+
 export default App;
